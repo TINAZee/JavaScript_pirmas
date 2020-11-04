1a8ef5d29473b3ebb2a5fd03f0f0a3b2f70da77f
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-11-04 15:58:06 +0200

    kvadratine lygtis

--- pamoka1.js
+++ pamoka1.js
@@ -1,1 +1,26 @@
 console.log("Hello World !!!");
+
+//kvadratines lygties programa
+
+var a = 0;
+var b = 0;
+var c = 0;
+
+if (a !== 0) {
+    var d = b * b - 4 * a * c;
+    if (d < 0) {
+        console.log("lygtis saknu neturi");
+    } else if (d === 0) {
+        console.log("vienas sprendinys", b / (2 * a));
+    } else {
+        var x1 = (b - Math.sqrt(b * b- 4 * a *c))/(2 * a);
+        var x2 = (b + Math.sqrt(b * b- 4 * a *c))/(2 * a);
+        console.log(x1);
+        console.log(x2);
+    }
+
+} else {
+    console.log("ne kvadratine lygtis");
+}
+
+//ax2 + bx + c = 0;
