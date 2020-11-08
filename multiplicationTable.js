fef74c0689fa298b79ff658f68d5756e2efde659
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-11-08 18:40:06 +0200

    multiplication table

---
+++ multiplicationTable.js
@@ -0,0 +1,31 @@
+let result = ' x   ';
+
+function space(value){
+  let buff = '';
+  let pad = 4 - value;
+    while( pad-- > 0 )
+            buff += ' ';            
+  return buff;
+}
+
+let from = 1;
+let to = 10;
+
+for (let i = from-1; i < to+1; i++) {
+
+    for (let j = from-1; j < to+1; j++) {
+
+        if(i == 0 && j > 0){
+          result += '[' + j + ']' + space((j+'').length+2);
+        } 
+        else if(j == 0 && i>0){
+          result += '[' + i + ']';
+        } 
+        else if(i>0 && j>0){
+        result += space((i*j+'').length ) + i*j;
+        }
+    }
+    result += '\n'
+}
+
+console.log(result);
