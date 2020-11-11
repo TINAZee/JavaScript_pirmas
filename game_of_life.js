f5763ec3307750ef0d7938b1aaecbcebe5114044
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-11-11 02:52:26 +0200

    50 TIMES

--- game_of_life.js
+++ game_of_life.js
@@ -1,11 +1,11 @@
 
 
 let field = [
-[".", "X", ".", ".", "."],
-["X", "X", ".", ".", "."],
-[".", ".", "X", "X", "X"],
-[".", ".", ".", "X", "X"],
-[".", ".", ".", "X", "."]
+[".", ".", ".", ".", "X"],
+[".", ".", "X", ".", "."],
+[".", ".", "X", "X", "."],
+[".", ".", "X", ".", "."],
+[".", ".", ".", ".", "."]
 ];
 
 let field2 = [
@@ -44,57 +44,57 @@ let NeighborCount = 0;
 let outOfBorder = 0
 let NeighborCountField = [];
 
+for (let p = 0; p <= 50; p++){
 
-
-for (let i = 0; i < field.length; i++) {
-    for (let j = 0; j < field[i].length; j++) {
-        for (let x = -1; x <= 1; x++) {
-            for (let y = -1; y <= 1; y++) {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            for (let x = -1; x <= 1; x++) {
+                for (let y = -1; y <= 1; y++) {
+                    
+                if (i+x < 0 || j+y < 0 || i+x >= field.length || j+y >= field.length || x == 0  && y == 0) {
+                } 
+                else  {
+                        if (field[i+x][j+y] === "X"){
+                            NeighborCount++
+                            
+                        }
+                }
                 
-               if (i+x < 0 || j+y < 0 || i+x >= field.length || j+y >= field.length || x == 0  && y == 0) {
-               } 
-               else  {
-                    if (field[i+x][j+y] === "X"){
-                        NeighborCount++
-                        
-                    }
-               }
-               
+                }
+            }
+            field2[i][j] = NeighborCount;
+            NeighborCount = 0;
+        }
+    }
+
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            let neighbors = field2[i][j];
+
+            if (field[i][j] == "X") {
+                
+                if (neighbors < 2) {
+                    field[i][j] = ".";
+                } else if (neighbors == 2 || neighbors == 3) {
+                    field[i][j] = "X";
+                } else if (neighbors > 3) {
+                    field[i][j] = ".";
+                }
+            } else if (field[i][j] == ".") {
+            
+                if (neighbors == 3) {
+                    field[i][j] = "X";
+                }
             }
         }
-        field2[i][j] = NeighborCount++;
-        NeighborCount = 0;
-        outOfBorder = 0;
     }
+    for (let i = 0; i < field.length; i++) {
+        let line = i + ": ";
+        for (let j = 0; j < field[i].length; j++) {
+            line += field[i][j];
+        }
+        console.log(line);
+    }
+    console.log("-----------------------");
 }
 
-console.log(field2);
-
-
-
-
-
-// if(field[i-1][j-1] === "X") {
-//     NeighborCount++
-// }
-// if(field[i-1][j+1] === "X") {
-//     NeighborCount++
-// }
-// if(field[i-1][j+2] === "X") {
-//     NeighborCount++
-// }
-// if(field[i][j-1] === "X") {
-//     NeighborCount++
-// }
-// if(field[i][j+2] === "X") {
-//     NeighborCount++
-// }
-// if(field[i+1][j-1] === "X") {
-//     NeighborCount++
-// }
-// if(field[i+1][j+1] === "X") {
-//     NeighborCount++
-// }
-// if(field[i+1][j+2] === "X") {
-//     NeighborCount++
-// }
