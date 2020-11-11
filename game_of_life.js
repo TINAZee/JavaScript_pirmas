d18a6e811053c3f78197b4b8bb27ab094fe1f4e3
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-11-12 00:51:23 +0200

    game update

--- game_of_life.js
+++ game_of_life.js
@@ -1,13 +1,4 @@
 
-
-// let field = [
-// [".", ".", ".", ".", "."],
-// [".", ".", "X", ".", "."],
-// [".", ".", "X", ".", "."],
-// [".", ".", "X", ".", "."],
-// [".", ".", ".", ".", "."]
-// ];
-
 const size = 10;
 let field = [];
 for (let i = 0; i < size; i++) {
@@ -34,75 +25,115 @@ console.log("-----------------------");
 
 let NeighborCount = 0;
 
-const NeighborCountFieldSize = 10;
 let NeighborCountField = [];
-for (let i = 0; i < NeighborCountFieldSize; i++) {
+for (let i = 0; i < size; i++) {
     const NeighborCountFieldSizeLine = [];
 
     for (let j = 0; j < size; j++) {
-        if (Math.random() < 0.25) {
-            NeighborCountFieldSizeLine.push("X");
-        } else {
-            NeighborCountFieldSizeLine.push(".");
-        } 
+        NeighborCountFieldSizeLine.push(0);
     }
     NeighborCountField.push(NeighborCountFieldSizeLine);
 }
 
-let p = 0;
-let go = true;
+let TEST = [];
+let field2 = field;
+let similarity = 0;
+let printCount = 0;
+let maxIterations = 50;
 
-for (let p = 0; p <= 50; p++){
-
-    for (let i = 0; i < field.length; i++) {
-        for (let j = 0; j < field[i].length; j++) {
-            for (let x = -1; x <= 1; x++) {
-                for (let y = -1; y <= 1; y++) {
-                    
-                if (i+x < 0 || j+y < 0 || i+x >= field.length || j+y >= field.length || x == 0  && y == 0) {
-                } 
-                else  {
-                        if (field[i+x][j+y] === "X"){
-                            NeighborCount++
-                            
+for (let p = 0; p <= maxIterations; p++){
+        //count neighbors
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i].length; j++) {
+                for (let x = -1; x <= 1; x++) {
+                    for (let y = -1; y <= 1; y++) {
+                        
+                    if (i+x < 0 || j+y < 0 || i+x >= field.length || j+y >= field.length || x == 0  && y == 0) {
+                    } 
+                    else  {
+                         if (field[i+x][j+y] === "X"){
+                             NeighborCount++
                         }
+                    }
+                    }
                 }
-                
-                }
+                NeighborCountField[i][j] = NeighborCount;
+                NeighborCount = 0;
             }
-            NeighborCountField[i][j] = NeighborCount;
-            NeighborCount = 0;
         }
-    }
-
-    for (let i = 0; i < field.length; i++) {
-        for (let j = 0; j < field[i].length; j++) {
-            let neighbors = NeighborCountField[i][j];
-
-            if (field[i][j] == "X") {
+        
+        //put value in to next 2d array
+        let TESTsizeArray = [];
+        for (let i = 0; i < field2.length; i++) {
+            let TESTsizeLine =[];
+            for (let j = 0; j < field2[i].length; j++) {
+                let neighbors = NeighborCountField[i][j];
+                let a = field2[i][j];
+                if (field2[i][j] == "X") {
+                    
+                    if (neighbors < 2) {
+                        field2[i][j] = ".";
+                    } else if (neighbors == 2 || neighbors == 3) {
+                        field2[i][j] = "X";
+                    } else if (neighbors > 3) {
+                        field2[i][j] = ".";
+                    }
+                } else if (field2[i][j] == ".") {
                 
-                if (neighbors < 2) {
-                    field[i][j] = ".";
-                } else if (neighbors == 2 || neighbors == 3) {
-                    field[i][j] = "X";
-                } else if (neighbors > 3) {
-                    field[i][j] = ".";
+                    if (neighbors == 3) {
+                        field2[i][j] = "X";
+                    }
+
                 }
-            } else if (field[i][j] == ".") {
+                let b = field2[i][j]
+                if(a===b){
+                similarity++ 
+                }
+                
+                if (b == "X"){
+                    TESTsizeLine.push("X")
+                }else {
+                    TESTsizeLine.push(".")
+                }
+               
+            }
+            TESTsizeArray.push(TESTsizeLine);
+        }
+        TEST.push(TESTsizeArray);
+        if (similarity === size*size){
+            break
+        }
+        
+        similarity = 0;
+        //prints 2d array
+        printCount++ 
+        for (let i = 0; i < field2.length; i++) {
+            let line = i + ": ";
+            for (let j = 0; j < field2[i].length; j++) {
             
-                if (neighbors == 3) {
-                    field[i][j] = "X";
-                }
+                line += field2[i][j];
+            
             }
+            console.log(line);
         }
-    }
-    for (let i = 0; i < field.length; i++) {
+        console.log("-----------------------");
+}
+let x=0
+console.log("Atspausdintas iteracijų skaičius:",printCount);
+
+for (let p = 0; p < printCount; p++) {
+    for (let i = 0; i < size; i++) {
         let line = i + ": ";
-        for (let j = 0; j < field[i].length; j++) {
-            line += field[i][j];
+        for (let j = 0; j < size; j++) {
+        
+            line += TEST[p][i][j];
+        
         }
         console.log(line);
+
     }
-    console.log("-----------------------");
+    x++
+    console.log("-----------------------",x);
 }
+
 
